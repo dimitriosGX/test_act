e0dcf4797726b49ea54b1724fade44b2e96e3a17
Dimitrios Karkoulis <dimitrios@gatlingx.com>
2024-10-09 13:17:18 +0300

    refac

--- src/execute.sh
+++ src/execute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 $ACTION_PATH/src/compress.sh
-pip3 run python $ACTION_PATH/src/bot_api.py --bot_address $1 --bot_port $2 --token $3
+python3 $ACTION_PATH/src/bot_api.py --bot_address $1 --bot_port $2 --token $3
