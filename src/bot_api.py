34c859e7f65589036dfd858445f07c31511d4e4f
Dimitrios Karkoulis <dimitrios@gatlingx.com>
2024-10-10 18:02:51 +0300

    try echo

--- src/bot_api.py
+++ src/bot_api.py
@@ -5,19 +5,19 @@ import json
 import argparse  
 import requests
 
-def test_token(bot_address, bot_port, token):
-    url = f"http://{bot_address}:{bot_port}/api/token"
-    headers = {'Authorization': f"Bearer {token}"}
+def authenticate(bot_address, bot_port, api_key):
+    url = f"http://{bot_address}:{bot_port}/api/authenticate"
+    headers = {'X-API-KEY': f"{api_key}"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return True
     else:
         raise Exception(f"Failed: {response.status_code}")
 
-def transfer(bot_address, bot_port, token):
+def transfer(bot_address, bot_port, api_key):
         
     url = f"http://{bot_address}:{bot_port}/api/transfer"
-    headers = {'Authorization': f"Bearer {token}"}
+    headers = {'X-API-KEY': f"{api_key}"}
     files = {'file': ('src.zip', open('src.zip', 'rb'), 'application/zip')}
     data = {'repo_url': f"https://github.com/${{ github.repository }}"}
     
@@ -25,7 +25,7 @@ def transfer(bot_address, bot_port, token):
     if response.status_code != 200:
         raise Exception(f"Failed: {response.status_code}: {response.text}")
 
-def hack(bot_address, bot_port, token):
+def hack(bot_address, bot_port, api_key):
     async def make_request(session, url, data, headers):
         headers["Content-Type"] = "application/json"
         async with session.post(url, json=data, headers=headers) as response:
@@ -45,7 +45,7 @@ def hack(bot_address, bot_port, token):
 
     async def main():
         url = f"http://{bot_address}:{bot_port}/api/hackv2"
-        headers = {'Authorization': f"Bearer {token}"}
+        headers = {'X-API-KEY': f"{api_key}"}
         data = {"repo_url": f"https://github.com/${{ github.repository }}"}
 
         async with aiohttp.ClientSession() as session:
@@ -69,26 +69,36 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--bot_address", type=str, required=True)
     parser.add_argument("--bot_port", type=str, required=True)
-    parser.add_argument("--token", type=str, required=True)
+    parser.add_argument("--api_key", type=str, required=True)
+    parser.add_argument("--authenticate", action="store_true")
     args = parser.parse_args()
 
+
+    if args.authenticate:
+        try:
+            authenticate(args.bot_address, args.bot_port, args.api_key)
+        except Exception as e:
+            print(e)
+            exit(1)
+        exit(0)
+
     try:
-        transfer(args.bot_address, args.bot_port, args.token)
+        transfer(args.bot_address, args.bot_port, args.api_key)
     except Exception as e:
         print(e)
         exit(1)
 
     try:
-        results = hack(args.bot_address, args.bot_port, args.token)
+        results = hack(args.bot_address, args.bot_port, args.api_key)
         
-        # Get GITHUB_OUTPUT environment variable
-        github_output = os.environ.get('GITHUB_OUTPUT')
+        # # Get GITHUB_OUTPUT environment variable
+        # github_output = os.environ.get('GITHUB_OUTPUT')
         
-        if github_output:
-            with open(github_output, 'a') as f:
-                f.write(f"results={json.dumps(results)}\n")
-        else:
-            print(f"{json.dumps(results)}\n")
+        # if github_output:
+        #     with open(github_output, 'a') as f:
+        #         f.write(f"results={json.dumps(results)}\n")
+        # else:
+        print(f"{json.dumps(results)}\n")
         
     except Exception as e:
         print(e)
